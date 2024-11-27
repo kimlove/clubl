057e90907e5b84b9997d48c603ac97c1385d9e8b
Kim Love <kim@kimlove.com>
2024-11-27 19:44:57 +0000

    chore: Added API url settings and stubbed fetchtData function

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { useEffect, useState } from "react";
+
+// settings like this normally better in a config object or populated via .env vars
+// Inline these for now
+const cryptoUrl = "https://api.coincap.io/v2/assets";
+const currencyUrl = "https://open.er-api.com/v6/latest/USD";
+const cryptoRowLimit = 25; // max number of crypto rows we want
+
 export const App = () => {
-  return (
-    <main>
-      <h1>Hello</h1>
-      <p>Welcome to this barebones Vite/React starter project.</p>
-    </main>
-  );
+  const [crypto, setCrypto] = useState(null);
+  const [currencies, setCurrencies] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      console.log("Fetch Data:");
+      console.log("cryptoUrl", cryptoUrl);
+      console.log("currencyUrl", currencyUrl);
+    };
+
+    fetchData();
+  }, []);
+
+  return <main>Display top 100 Crypto Currencies</main>;
 };
