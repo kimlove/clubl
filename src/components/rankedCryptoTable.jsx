08f9fe14dad7ec9d582ee158d18be42437ce3b07
Kim Love <kim@kimlove.com>
2024-11-27 21:49:25 +0000

    feat: Added GBP/EUR/AED currency conversions using using currency API data

--- src/components/rankedCryptoTable.jsx
+++ src/components/rankedCryptoTable.jsx
@@ -1,4 +1,5 @@
 export const RankedCryptoTable = ({ crypto, currencies }) => {
+  const { GBP, EUR, AED } = currencies.rates; // destructure currency rates and just grab the ones we need
   return (
     <>
       <table>
@@ -22,7 +23,14 @@ export const RankedCryptoTable = ({ crypto, currencies }) => {
                     {row.symbol}
                   </a>
                 </td>
-                <td>US$ {row.priceUsd}</td>
+                <td>
+                  <div>US$ {row.priceUsd}</div>
+                  <ul>
+                    <li>GBP: {row.priceUsd * GBP}</li>
+                    <li>EUR: {row.priceUsd * EUR}</li>
+                    <li>AED: {row.priceUsd * AED}</li>
+                  </ul>
+                </td>
                 <td>{row.changePercent24Hr}%</td>
               </tr>
             );
