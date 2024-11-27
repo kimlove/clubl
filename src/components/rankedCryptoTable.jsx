b2a02a224d0572e7e3b3e9838ae881d7413a802a
Kim Love <kim@kimlove.com>
2024-11-27 22:10:17 +0000

    feat: Added sortCrypto function

--- src/components/rankedCryptoTable.jsx
+++ src/components/rankedCryptoTable.jsx
@@ -1,42 +1,60 @@
+import { useEffect, useState } from "react";
+
+import { sortCrypto } from "../lib/sortCrypto";
+
 export const RankedCryptoTable = ({ crypto, currencies }) => {
+  const [sortedCrypto, setSortedCrypto] = useState(null);
+  // default to rank ascending
+  const [sortBy, setSortBy] = useState({
+    column: "rank",
+    order: "ASC",
+  });
+
+  useEffect(() => {
+    setSortedCrypto(sortCrypto(crypto, sortBy.column, sortBy.order));
+  }, []);
+
   const { GBP, EUR, AED } = currencies.rates; // destructure currency rates and just grab the ones we need
+
   return (
     <>
-      <table>
-        <thead>
-          <tr>
-            <th>Rank</th>
-            <th>ID</th>
-            <th>Symbol</th>
-            <th>Price</th>
-            <th>24hr Change</th>
-          </tr>
-        </thead>
-        <tbody>
-          {crypto.map((row) => {
-            return (
-              <tr key={row.symbol}>
-                <td>{row.rank}</td>
-                <td>{row.id}</td>
-                <td>
-                  <a href={row.explorer} target="_blank">
-                    {row.symbol}
-                  </a>
-                </td>
-                <td>
-                  <div>US$ {row.priceUsd}</div>
-                  <ul>
-                    <li>GBP: {row.priceUsd * GBP}</li>
-                    <li>EUR: {row.priceUsd * EUR}</li>
-                    <li>AED: {row.priceUsd * AED}</li>
-                  </ul>
-                </td>
-                <td>{row.changePercent24Hr}%</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      {sortedCrypto ? (
+        <table>
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th>ID</th>
+              <th>Symbol</th>
+              <th>Price</th>
+              <th>24hr Change</th>
+            </tr>
+          </thead>
+          <tbody>
+            {sortedCrypto.map((row) => {
+              return (
+                <tr key={row.symbol}>
+                  <td>{row.rank}</td>
+                  <td>{row.id}</td>
+                  <td>
+                    <a href={row.explorer} target="_blank">
+                      {row.symbol}
+                    </a>
+                  </td>
+                  <td>
+                    <div>US$ {row.priceUsd}</div>
+                    <ul>
+                      <li>GBP: {row.priceUsd * GBP}</li>
+                      <li>EUR: {row.priceUsd * EUR}</li>
+                      <li>AED: {row.priceUsd * AED}</li>
+                    </ul>
+                  </td>
+                  <td>{row.changePercent24Hr}%</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      ) : null}
     </>
   );
 };
---
+++ src/lib/sortCrypto.js
@@ -0,0 +1,21 @@
+// normally you'd want to unit test business logic like this with Jest or similar, but skip for now
+
+export const sortCrypto = (data, column, order) => {
+  const sortedData = [...data].sort((a, b) => {
+    let aVal = column === "price" ? parseFloat(a.priceUsd) : a[column];
+    let bVal = column === "price" ? parseFloat(b.priceUsd) : b[column];
+
+    // treat id and rank as numbers so we can sort correctly
+    if (column === "id" || column === "rank") {
+      aVal = parseFloat(aVal);
+      bVal = parseFloat(bVal);
+    }
+
+    // sort depending on the order
+    if (aVal < bVal) return order === "ASC" ? -1 : 1;
+    if (aVal > bVal) return order === "ASC" ? 1 : -1;
+    return 0;
+  });
+
+  return sortedData;
+};
